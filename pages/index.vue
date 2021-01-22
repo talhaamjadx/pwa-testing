dd907bfac525d40db06b5ddee89894e049fee119
talha amjad <talha.amjad@techandover.com>
2021-01-22 17:11:19 +0500

    container removed

--- pages/index.vue
+++ pages/index.vue
@@ -15,6 +15,12 @@ export default {
       response: ""
     }
   },
+  mounted(){
+    window.navigator.geolocation.getCurrentPosition(console.log)
+    window.addEventListener('devicemotion', (event) => {
+      console.log(event)
+    })
+  },
   methods:{
     async connectBluetooth(){
       const device = await navigator.bluetooth.requestDevice({ acceptAllDevices: true }).then(res => {
@@ -46,42 +52,4 @@ export default {
 }
 </script>
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
